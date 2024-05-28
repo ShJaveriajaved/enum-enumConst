c60a33349631bb52969418ff55159b74c5a5f9c1
ShJaveriajaved <161949130+ShJaveriajaved@users.noreply.github.com>
2024-05-29 01:15:11 +0500

    Add files via upload
    look for Enum and ConstEnum topic

---
+++ main.js
@@ -0,0 +1,18 @@
+//Enum
+//  const Direction = {
+//     up:0,
+//     down:1,
+//     left:2,
+//     right:3,
+//  }
+//     let currentDirection = Direction.down
+//     console.log(currentDirection);
+//Enum 
+var ConstDirection = ({
+    up: 0,
+    down: 1,
+    left: 2,
+    right: 3,
+});
+var currentConstDirection = ConstDirection.down;
+console.log(currentConstDirection);
---
+++ main.ts
@@ -0,0 +1,26 @@
+                    //Enum
+
+
+//  const Direction = {
+//     up:0,
+//     down:1,
+//     left:2,
+//     right:3,
+            
+//  }
+//     let currentDirection = Direction.down
+//     console.log(currentDirection);
+    
+
+ 
+                    //Enum 
+ const ConstDirection = ({
+   up:0,
+   down:1,
+   left:2,
+   right:3,
+  });
+
+  let currentConstDirection =
+  ConstDirection.down;
+  console.log(currentConstDirection);
